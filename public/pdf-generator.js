4ece7248095a95804bcbc96deda52cfdca7a8421
Abhijeet Pandit <abhijeetpandit7@gmail.com>
2021-03-13 16:05:33 +0530

    Fix structure

--- public/pdf-generator.js
+++ public/pdf-generator.js
@@ -1,22 +1,22 @@
 window.onload = function () {
+    document.getElementById("download-receipt")
+    .addEventListener("click", () => {
+        const invoice = this.document.getElementById("receipt");
+        var opt = {
+            margin: 1,
+            filename: 'receipt.pdf',
+            image: { type: 'jpeg', quality: 1 },
+            html2canvas: { scale: 2 },
+            jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+        };
+        html2pdf().from(invoice).set(opt).save();
+    })
     document.getElementById("download-btn")
         .addEventListener("click", () => {
             const invoice = this.document.getElementById("print-content");
             var opt = {
                 margin: 1,
                 filename: 'bill.pdf',
-                image: { type: 'jpeg', quality: 1 },
-                html2canvas: { scale: 2 },
-                jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
-            };
-            html2pdf().from(invoice).set(opt).save();
-        })
-    document.getElementById("download-receipt")
-        .addEventListener("click", () => {
-            const invoice = this.document.getElementById("receipt");
-            var opt = {
-                margin: 1,
-                filename: 'receipt.pdf',
                 image: { type: 'jpeg', quality: 1 },
                 html2canvas: { scale: 2 },
                 jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
